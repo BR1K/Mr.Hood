cf23e01cf1248670eb9686f7f1b5bd02da98ed77
Brikend Behrami <brikendbehrami@gmail.com>
2018-07-10 18:17:31 -0400

    fix syntax error

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -12,7 +12,7 @@ class Api::UsersController < ApplicationController
 
 	private
 	def user_params
-		params.require(:user).permit(:first_name, :last_name, :email, :username :password)
+		params.require(:user).permit(:first_name, :last_name, :email, :username, :password)
 	end
 
 end
