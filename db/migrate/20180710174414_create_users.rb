ad4687e35c9360588c4ebc5cf86b6c17d7085987
Brikend Behrami <brikendbehrami@gmail.com>
2018-07-10 17:14:34 -0400

    user table migration

--- db/migrate/20180710174414_create_users.rb
+++ db/migrate/20180710174414_create_users.rb
@@ -1,6 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :email, null: false
       t.string :username
       t.string :password_digest, null: false
       t.string :session_token, null: false
@@ -9,6 +12,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     end
 
     add_index :users, :username, unique: true
+    add_index :users, :email, unique: true
     add_index :users, :session_token, unique: true
   end
 end
