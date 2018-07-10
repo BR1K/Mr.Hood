d3d64634d60696d057ed781b07bcd087f57b5453
Brikend Behrami <brikendbehrami@gmail.com>
2018-07-10 17:12:00 -0400

    finish webpack/ package.json setup

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/MrHood.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: './bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['env', 'react']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
