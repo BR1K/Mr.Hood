a6f308634cc550deee4734df98c2b97ff3d6d61d
Brikend Behrami <brikendbehrami@gmail.com>
2018-07-11 14:21:00 -0400

    add :show route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
 		resources :users, only: [:create]
-		resource :session, only: [:create, :destroy]
+		resource :session, only: [:create, :destroy, :show]
   end
 end
