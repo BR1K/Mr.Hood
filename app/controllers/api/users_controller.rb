c5dc115e933b41be8e0a8ad89e1052ba4fb7c620
Brikend Behrami <brikendbehrami@gmail.com>
2018-07-11 14:18:34 -0400

    refactor; change render in users_controller create method

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -4,7 +4,7 @@ class Api::UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save
 			login(@user)
-			render :show
+			render 'api/users/show'
 		else
 			render json: @user.errors.full_messages, status: 401
 		end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,28 @@
 class ApplicationController < ActionController::Base
 
-  helper_method :current_user
-  helper_method :logged_in?
+  helper_method :current_user, :logged_in?
 
 	def current_user
+    return nil unless session[:session_token]
 		@current_user ||= User.find_by(session_token: session[:session_token])
 	end
 
-	# def ensure_logged_in
-	# 	redirect_to unless logged_in?
-	# end
+  def ensure_logged_in
+    unless current_user
+      render json: { base: ['invalid credentials'] }, status: 401
+    end
+  end
 
 	def login(user)
-		@current_user = user
-		session[:session_token] = @current_user.session_token
+    user.reset_session_token!
+    session[:session_token] = user.session_token
+    @current_user = user
 	end
 
 	def logout
-		@current_user.reset_session_token
-		session[:session_token] = nil
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    @current_user = nil
 	end
 
 	def logged_in?
