fa8ee584d008ad1c5f6b96e9bb03e70f4d0748e0
Brikend Behrami <brikendbehrami@gmail.com>
2018-07-11 14:21:32 -0400

    bootstrap current user

--- frontend/MrHood.jsx
+++ frontend/MrHood.jsx
@@ -1,1 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './components/root';
+import configureStore from './store/store';
 
+
+document.addEventListener('DOMContentLoaded', () => {
+  let store;
+
+  if (window.currentUser) {
+    const preloadedState = {
+      session: { id: window.currentUser.id },
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={store} />, root);
+});
