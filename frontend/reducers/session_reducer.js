d64416c0750ad8c5baeb088f9b3f44534e9aa9c6
Brikend Behrami <brikendbehrami@gmail.com>
2018-07-11 16:29:11 -0400

    replace merge with object.assign

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,14 +1,13 @@
 import { RECEIVE_CURRENT_USER,
 				 LOGOUT_CURRENT_USER,
 				 RECEIVE_ERRORS } from '../actions/session_actions';
-import merge from 'lodash/merge';
 
 export default (state = {}, action) => {
 	Object.freeze(state);
 	const newState = merge({}, state);
 	switch (action.type) {
 		case RECEIVE_CURRENT_USER:
-			merge(newState, {id: action.currentUser.id});
+			Object.assign(newState, {id: action.currentUser.id});
 			return newState;
 		case LOGOUT_CURRENT_USER:
 			newState.id = null;
