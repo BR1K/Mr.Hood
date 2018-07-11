424db4a4adfbaa2719a3798279ce7ece2af91055
Brikend Behrami <brikendbehrami@gmail.com>
2018-07-11 14:43:24 -0400

    undo prev change

--- frontend/components/session_form/login_form_container.jsx
+++ frontend/components/session_form/login_form_container.jsx
@@ -7,8 +7,7 @@ import SessionForm from './session_form';
 const mapStateToProps = ({ errors }) => {
   return {
     errors: errors.session,
-    formType: 'login',
-    navLink: <Link to="/signup">Sign Up</Link>,
+    formType: 'login'
   };
 };
 
