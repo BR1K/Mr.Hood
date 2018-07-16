155834efe0896480f67750cd3ddb69bfcec2232e
Brikend Behrami <brikendbehrami@gmail.com>
2018-07-16 09:14:23 -0400

    edit protected routes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -13,19 +13,18 @@ import SignUpFormContainer from './session_form/signup_form_container';
 import LogInFormContainer from './session_form/login_form_container';
 // import SearchContainer from './search/search_container';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
-import GreetingContainer2 from './greeting/greeting_container2'
+import GreetingContainer2 from './greeting/greeting_container2';
+import Dashboard from './dashboard/dashboard_container';
+import StockDetailContainer from './stock_detail/stock_detail_container';
 
 const App = () => (
   <div className="app">
-    <header>
-      <Link to="/" className="header-link">
-      </Link>
-      <Route exact path="/" component={GreetingContainer2} />
-    </header>
-
     <Switch>
+      <ProtectedRoute exact path="/" component={Dashboard} />
+      <ProtectedRoute exact path="stocks/:stockId" component={StockDetailContainer} />
       <AuthRoute exact path="/login" component={LogInFormContainer} />
       <AuthRoute exact path="/signup" component={SignUpFormContainer} />
+      <AuthRoute exact path="/welcome" component={GreetingContainer2} />
     </Switch>
   </div>
 );
@@ -33,3 +32,5 @@ const App = () => (
 export default App;
 
 // <h1>Mr.Hood</h1>
+
+// <AuthRoute exact path="/" component={GreetingContainer2} />
