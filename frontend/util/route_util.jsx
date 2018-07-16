3c84633926ec2c70a9b4f015479b2dc0a615863c
Brikend Behrami <brikendbehrami@gmail.com>
2018-07-16 09:14:09 -0400

    change welcome page url to '/welcome'

--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect, withRouter } from 'react-router-dom';
+import Greeting from '../components/greeting/greeting_container2';
 
 const Auth = ({ component: Component, path, loggedIn, exact }) => (
   <Route path={path} exact={exact} render={(props) => (
@@ -17,7 +18,7 @@ const Protected = ({ component: Component, path, loggedIn, exact }) => (
      loggedIn ? (
       <Component {...props} />
     ) : (
-      <Redirect to="/login" />
+      <Redirect to="/welcome" />
     )
   )} />
 );
