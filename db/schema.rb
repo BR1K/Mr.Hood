10179e2997e0915a80ad4635e5cbb4eb3d4a8a1e
Brikend Behrami <brikendbehrami@gmail.com>
2018-07-17 22:25:21 -0400

    seed db with valid IEX stock symbols

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_10_174414) do
+ActiveRecord::Schema.define(version: 2018_07_18_015412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "portfolio_snapshots", force: :cascade do |t|
+    t.integer "portfolio_id", null: false
+    t.string "date", null: false
+    t.float "value", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["portfolio_id"], name: "index_portfolio_snapshots_on_portfolio_id"
+  end
+
+  create_table "portfolios", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_portfolios_on_user_id", unique: true
+  end
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "symbol", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["symbol"], name: "index_stocks_on_symbol", unique: true
+  end
+
+  create_table "trades", force: :cascade do |t|
+    t.integer "stock_id", null: false
+    t.integer "portfolio_id", null: false
+    t.float "price", null: false
+    t.integer "size", null: false
+    t.string "trade_type", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["portfolio_id"], name: "index_trades_on_portfolio_id"
+    t.index ["stock_id"], name: "index_trades_on_stock_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name", null: false
@@ -24,9 +59,19 @@ ActiveRecord::Schema.define(version: 2018_07_10_174414) do
     t.string "session_token", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "buying_power"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["session_token"], name: "index_users_on_session_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
+  end
+
+  create_table "watchlist_items", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "stock_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["stock_id"], name: "index_watchlist_items_on_stock_id", unique: true
+    t.index ["user_id"], name: "index_watchlist_items_on_user_id", unique: true
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+User.create(id: 1, first_name: "Robin", last_name: "Hood", email: "rhood@mrhood.com", password: "Hunter12" )
+
+
+symbols = File.join(Rails.root, 'db', 'iex_stocks', 'IEX_Stocks.csv')
+
+
+CSV.foreach(symbols) do |row|
+    Stock.create!({symbol: row[0]})
+end
