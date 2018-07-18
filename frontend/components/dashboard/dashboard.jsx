24302204fe077e3268c8fcf31118fc98394d5b97
Brikend Behrami <brikendbehrami@gmail.com>
2018-07-17 22:36:22 -0400

    begin data visualisation of alphavantage stock data using recharts api

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -9,31 +9,43 @@ class Dashboard extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = [];
+    this.state = {
+      stocks: [],
+    };
 
   }
 
   componentDidMount() {
-    const user = window.currentUser;
+    console.log('hello world')
+    const user = this.props.currentUser;
     // User object is available
-    console.log(window.currentUser);
+    console.log(user);
     // const { stockWatchList = ['AAPL', 'MSFT'] } = user;
     const stockWatchList = ['AAPL', 'MSFT'];
 
     Promise.all(
       stockWatchList.map(stockSym => {
-        return fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stockSym}&interval=1min&apikey=PK7DV11EJ8OQEYVU`)
+        return fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stockSym}&interval=1min&apikey=PK7DV11EJ8OQEYVU`)
         .then(res => res.json())
       })
     ).then(stockResponses => {
 
-      console.log(stockResponses)
-
-    })
+//      console.log(Object.keys(stockResponses[0], stockResponses[0]['Time Series (1min)']), '<<<<<')
+      const data = Object.keys(stockResponses[0]['Time Series (Daily)']).map(key => {
+        return {
+          name: key,
+          value: parseFloat(stockResponses[0]['Time Series (Daily)'][key]["3. low"])
+        }
+      })
+      console.log('>>>>>>', data)
+      this.setState({stocks: data })
+    //console.log(data);
+      return data;
+    }).then(res => console.log(res))
   }
 
-
   render() {
+
     return (
       <div>
         <div>
@@ -41,14 +53,14 @@ class Dashboard extends React.Component {
           <button className="header-button" onClick={this.props.logout}>Log Out</button>
         </div>
         <div>
-          <DashboardChart></DashboardChart>
+          <DashboardChart data={this.state.stocks}></DashboardChart>
           <DashboardSidebar></DashboardSidebar>
         </div>
       </div>
     );
-  }
+  };
 
-}
+};
 
 
 
