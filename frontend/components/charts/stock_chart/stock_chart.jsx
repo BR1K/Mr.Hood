5e6bb144426ed76511163ce57a980e5b165d6ba3
Brikend Behrami <brikendbehrami@gmail.com>
2018-08-24 15:29:23 -0400

    add responsive container to chart

--- frontend/components/charts/stock_chart/stock_chart.jsx
+++ frontend/components/charts/stock_chart/stock_chart.jsx
@@ -119,20 +119,22 @@ class StockChart extends React.Component {
               <span className="range"></span>
             </div>
           </div>
-          <LineChart data={data} margin={{top:25, bottom: 25}} width={1150} height={400}>
-            <Line
-              type="linear"
-              dataKey="high"
-              strokeWidth={2} stroke="#21ce99"
-              dot={false}
-              isAnimationActive={true}
-            />
+          <ResponsiveContainer width="100%" height={400}>
+            <LineChart data={data} margin={{top:25, bottom: 25}} >
+              <Line
+                type="linear"
+                dataKey="high"
+                strokeWidth={2} stroke="#21ce99"
+                dot={false}
+                isAnimationActive={true}
+                />
 
-            <YAxis
-              hide={true}
-              domain={[min, max]}
-            />
-          </LineChart>
+              <YAxis
+                hide={true}
+                domain={[min, max]}
+                />
+            </LineChart>
+          </ResponsiveContainer>
           <div className="button-list">
             <ul>{rangeButtons}</ul>
           </div>
