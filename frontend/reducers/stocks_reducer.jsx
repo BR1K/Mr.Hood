797774486be0d1cfa2a82a12dc49db38b1a5d922
Brikend Behrami <brikendbehrami@gmail.com>
2018-09-21 16:11:11 -0400

    refactor stocks reducer

--- frontend/reducers/stocks_reducer.jsx
+++ frontend/reducers/stocks_reducer.jsx
@@ -1,4 +1,5 @@
 import { RECEIVE_STOCK, RECEIVE_STOCKS} from '../actions/stock_actions';
+import merge from 'lodash/merge';
 
 const stocksReducer = (state = {}, action) => {
   Object.freeze(state);
@@ -11,5 +12,17 @@ const stocksReducer = (state = {}, action) => {
       return state;
   }
 };
+
+// const stocksReducer = (state = {}, action) => {
+//   Object.freeze(state);
+//   switch(action.type) {
+//     case RECEIVE_STOCK:
+//       return merge({}, state, { [action.stock.symbol]: action.stock });
+//     case RECEIVE_STOCKS:
+//       return action.stocks;
+//     default:
+//       return state;
+//   }
+// };
 
 export default stocksReducer;
