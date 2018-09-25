7445a8fe697f6b0b2272510e1642da9c0f35e0ba
Brikend Behrami <brikendbehrami@gmail.com>
2018-09-25 13:16:09 -0400

    remove index on watchlist items on user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_21_003636) do
+ActiveRecord::Schema.define(version: 2018_09_25_030109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,7 +72,6 @@ ActiveRecord::Schema.define(version: 2018_09_21_003636) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["stock_id"], name: "index_watchlist_items_on_stock_id", unique: true
-    t.index ["user_id"], name: "index_watchlist_items_on_user_id", unique: true
   end
 
 end
