185789e113481d28424b5de490cab11707abea24
Brikend Behrami <brikendbehrami@gmail.com>
2018-09-24 21:53:04 -0400

    style watchlist button component

---
+++ frontend/components/stock_page/watchlist_button.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default ({ stock, watchStock, unwatchStock, watchlist }) => {
+
+  let buttonText;
+  let handleClick;
+
+  return (
+    <div className="watchlist-button">
+      Add to Watchlist
+    </div>
+  );
+};
