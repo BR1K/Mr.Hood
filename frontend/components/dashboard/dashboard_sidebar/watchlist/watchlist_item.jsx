50086cd0b83f2eba5c4a2d7104b24266c75ac4af
Brikend Behrami <brikendbehrami@gmail.com>
2018-09-27 16:51:11 -0400

    render stock price for each watchlist item

--- frontend/components/dashboard/dashboard_sidebar/watchlist/watchlist_item.jsx
+++ frontend/components/dashboard/dashboard_sidebar/watchlist/watchlist_item.jsx
@@ -20,3 +20,36 @@ class WatchlistItem extends React.Component {
     this.fetchChart = this.fetchChart.bind(this);
     this.fetchPrice = this.fetchPrice.bind(this);
   }
+
+  componentDidMount() {
+    this.fetchPrice(this.props.symbol);
+    this.setState({ loading: false });
+  }
+  fetchPrice(symbol) {
+    fetch(`https://api.iextrading.com/1.0/stock/${symbol}/price`)
+    .then(res => res.json())
+    .then(
+      (result) => {
+        this.setState({
+          price: { data: result }
+        });
+      },
+      (error) => {
+        this.setState({
+          chart: { error }
+        });
+      }
+    )
+  }
+
+  render() {
+    return(
+      <Link to={`/stocks/${this.props.symbol}`} className="portfolio-row">
+        <div>{this.props.symbol}</div>
+        <div>{this.state.price.data}</div>
+      </Link>
+    )
+  }
+}
+
+export default withRouter(WatchlistItem);
