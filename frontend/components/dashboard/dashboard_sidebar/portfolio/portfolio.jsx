0552eaa2e274c621d3939cf4ebd8b3159c862ecb
Brikend Behrami <brikendbehrami@gmail.com>
2018-09-30 16:23:43 -0400

    create separate portfolio component

--- frontend/components/dashboard/dashboard_sidebar/portfolio/portfolio.jsx
+++ frontend/components/dashboard/dashboard_sidebar/portfolio/portfolio.jsx
@@ -1,55 +1,105 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import PortfolioItem from './portfolio_item';
 
-const handleClick = (symbol, history) => {
-  return (e) => {
-    history.push(`/stocks/${symbol}`);
-  };
-};
+class Portfolio extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.portfolioItems = this.portfolioItems.bind(this);
+  }
 
-const renderTableRows = (holdings, stocks, history) =>  {
+  componentDidMount() {
+    this.props.fetchPortfolio()
+    .then(() => this.props.fetchStocks())
+  }
 
+  portfolioItems() {
+    let symbols = Object.keys(this.props.holdings);
+    let holdings = [];
+    for (let i = 0; i < symbols.length; i++) {
+      let symbol = symbols[i];
+      holdings.push (
+        <PortfolioItem
+          className="portfolio-row"
+          key={i}
+          symbol={symbol}
+          holdings={this.props.holdings[symbol]}
+        />
+      )
+    }
 
-  return (
-    Object.keys(holdings).map( (symbol, idx) => {
-      let numStocks = holdings[symbol];
-      if (numStocks > 0) {
-        return(
-          <div key={idx} className="portfolio-row"
-            onClick={handleClick(symbol, history)}>
-            <div className="stock-symbol">{symbol}</div>
-            <div className="portfolio-holdings">{numStocks} shares</div>
-            <div className="stock-price">
+    return holdings;
+  }
 
-            </div>
-          </div>
-        );
-      }
-    })
-  );
-};
+  render() {
 
-const renderStocks = (holdings, stocks, history) => {
-  //
-  return(
-      <div>
-        {renderTableRows(holdings, stocks, history)}
-      </div>
-  );
-};
+    return (
+      <ul className="watchlist">
+        <h3 className="portfolio-header">Portfolio</h3>
+        <div className="portfolio-body">
+          {this.portfolioItems()}
+        </div>
+      </ul>
+    )
+  }
+}
 
-const Portfolio = ({ holdings, stocks, history }) => {
-  //
-  return(
-    <aside className="portfolio">
-      <h3 className="portfolio-header">Portfolio</h3>
-      <div className="portfolio-body">
-        {renderStocks(holdings, stocks, history)}
-      </div>
-    </aside>
-  );
-};
+export default withRouter(Portfolio)
 
 
-export default withRouter(Portfolio);
+
+// import React from 'react';
+// import { Link, withRouter } from 'react-router-dom';
+//
+// const handleClick = (symbol, history) => {
+//   return (e) => {
+//     history.push(`/stocks/${symbol}`);
+//   };
+// };
+//
+//
+// const renderTableRows = (holdings, stocks, history) =>  {
+//
+//
+//   return (
+//     Object.keys(holdings).map( (symbol, idx) => {
+//       let numStocks = holdings[symbol];
+//       if (numStocks > 0) {
+//         return(
+//           <div key={idx} className="portfolio-row"
+//             onClick={handleClick(symbol, history)}>
+//             <div className="stock-symbol">{symbol}</div>
+//             <div className="portfolio-holdings">{numStocks} shares</div>
+//             <div className="stock-price">
+//             </div>
+//           </div>
+//         );
+//       }
+//     })
+//   );
+// };
+//
+// const renderStocks = (holdings, stocks, history) => {
+//   //
+//   return(
+//       <div>
+//         {renderTableRows(holdings, stocks, history)}
+//       </div>
+//   );
+// };
+//
+// const Portfolio = ({ holdings, stocks, history }) => {
+//   //
+//   return(
+//     <aside className="portfolio">
+//       <h3 className="portfolio-header">Portfolio</h3>
+//       <div className="portfolio-body">
+//         {renderStocks(holdings, stocks, history)}
+//       </div>
+//     </aside>
+//   );
+// };
+//
+//
+// export default withRouter(Portfolio);
