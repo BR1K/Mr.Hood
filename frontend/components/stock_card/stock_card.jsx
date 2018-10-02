e91bef9f18cfd56cdeb19c9620f9561729a15c21
Brikend Behrami <brikendbehrami@gmail.com>
2018-10-02 13:21:30 -0400

    refactor component did mount

--- frontend/components/stock_card/stock_card.jsx
+++ frontend/components/stock_card/stock_card.jsx
@@ -1,6 +1,7 @@
 import React    from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import StockCardChart1 from '../charts/stock_card_chart/stock_card_chart_container1';
+import { currencyFormatter } from '../../util/formatter';
 
 class StockCard extends React.Component {
 
@@ -22,13 +23,19 @@ class StockCard extends React.Component {
         error: null,
         loading: true,
       },
-      // loading: true,
+      quote: {
+        data: null,
+        error: null,
+        loading: true,
+      },
       refresh: null,
     };
 
     this.fetchChart = this.fetchChart.bind(this);
     this.fetchPrice = this.fetchPrice.bind(this);
     this.fetchCompany = this.fetchCompany.bind(this);
+    this.fetchQuote = this.fetchQuote.bind(this);
+    this.updatePrice = this.updatePrice.bind(this);
   }
 
 
@@ -37,23 +44,16 @@ class StockCard extends React.Component {
     this.fetchChart(this.props.symbol);
     this.fetchPrice(this.props.symbol);
     this.fetchCompany(this.props.symbol);
-    // this.props.fetchPeerPrice(this.props.peer)
-    //   .then(() => this.props.fetchPeerStats(this.props.peer))
-    //   .then(() => this.props.fetchPeerCompany(this.props.peer))
-    //   .then(() => this.props.fetchPeerChart(this.props.peer, "1D"))
-    //   .then(() => {
-    //     const refresh = setInterval(this.updatePrice, 5000);
-    //     this.setState({
-    //       refresh: refresh,
-    //       loading: false
-    //     });
-    //   }
-    // )
+    this.fetchQuote(this.props.symbol);
+    const refresh = setInterval(this.updatePrice, 5000)
+    this.setState({
+      refresh: refresh,
+    })
   }
 
-  // componentWillUnmount() {
-  //   clearInterval(this.state.refresh);
-  // }
+  componentWillUnmount() {
+    clearInterval(this.state.refresh);
+  }
 
   fetchChart(symbol) {
     fetch(`https://api.iextrading.com/1.0/stock/${symbol}/chart/1D`)
@@ -109,26 +109,45 @@ class StockCard extends React.Component {
         });
       }
     )
-    debugger
+  }
+
+  fetchQuote(symbol) {
+    fetch(`https://api.iextrading.com/1.0/stock/${symbol}/quote`)
+    .then(res => res.json())
+    .then(
+      (result) => {
+        this.setState({
+          quote: { data: result, loading: false }
+        });
+      },
+      (error) => {
+        this.setState({
+          quote: { error }
+        });
+      }
+    )
   }
 
 
-  // updatePrice() {
-  //
-  //   const currentPrice = this.props.price;
-  //   this.props.fetchPeerPrice(this.props.peer).then(
-  //     newPrice => {
-  //       if (currentPrice !== newPrice) {
-  //         this.props.price = newPrice
-  //       }
-  //     }
-  //   )
-  // }
+  updatePrice() {
+    this.fetchPrice(this.props.symbol)
+  }
 
   render() {
-    if (this.state.price.loading || this.state.chart.loading || this.state.company.loading) {
+    let kolor;
+
+    if (this.state.price.loading || this.state.chart.loading || this.state.company.loading || this.state.quote.loading) {
       return <div>loading...</div>
     } else {
+
+      let quote = this.state.quote.data;
+      if (quote.open > quote.latestPrice) {
+        kolor = '#f45531';
+      } else {
+        kolor = "#21ce99";
+      }
+      let priceColor = {color: kolor}
+
       return (
 
         <Link to={`/stocks/${this.props.symbol}`} className="stock-card">
@@ -139,8 +158,9 @@ class StockCard extends React.Component {
           <div className="stock-card-body">
             <StockCardChart1
               chart={this.state.chart.data}
+              kolor={kolor}
             />
-          <div className="stock-card-price">${this.state.price.data}</div>
+          <div style={priceColor} className="stock-card-price">{currencyFormatter.format(this.state.price.data)}</div>
           </div>
         </Link>
       )
