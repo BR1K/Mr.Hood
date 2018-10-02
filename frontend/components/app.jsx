9d78006276cea18a6c16caffd47658c9837cc019
Brikend Behrami <brikendbehrami@gmail.com>
2018-10-02 17:14:28 -0400

    rename imports

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -9,22 +9,22 @@ import {
 } from 'react-router-dom';
 
 
-import SignUpFormContainer from './session_form/signup_form_container';
-import LogInFormContainer from './session_form/login_form_container';
-import SearchContainer from './navbar/search/search_container';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
-import GreetingContainer2 from './greeting/greeting_container2';
+import SignUpPage from './session_form/signup_form_container';
+import LogInPage from './session_form/login_form_container';
+import SearchContainer from './navbar/search/search_container';
+import GreetingPage from './greeting/greeting_container2';
 import Dashboard from './dashboard/dashboard_container';
-import StockPageContainer from './stock_page/stock_page_container';
+import StockPage from './stock_page/stock_page_container';
 
 const App = () => (
   <div className="app">
     <Switch>
       <ProtectedRoute exact path="/" component={Dashboard} />
-      <ProtectedRoute exact path="/stocks/:symbol" component={StockPageContainer} />
-      <AuthRoute exact path="/login" component={LogInFormContainer} />
-      <AuthRoute exact path="/signup" component={SignUpFormContainer} />
-      <AuthRoute exact path="/welcome" component={GreetingContainer2} />
+      <ProtectedRoute exact path="/stocks/:symbol" component={StockPage} />
+      <AuthRoute exact path="/login" component={LogInPage} />
+      <AuthRoute exact path="/signup" component={SignUpPage} />
+      <AuthRoute exact path="/welcome" component={GreetingPage} />
     </Switch>
   </div>
 );
