678d090019ebf0df757f65a21ecb2da60fb75976
Brikend Behrami <brikendbehrami@gmail.com>
2018-10-03 15:16:30 -0400

    pass portfolioValue func from Sidebar to Portfolio

--- frontend/components/dashboard/dashboard_sidebar/dashboard_sidebar.jsx
+++ frontend/components/dashboard/dashboard_sidebar/dashboard_sidebar.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import Portfolio from './portfolio/portfolio_container';
 import Watchlist from './watchlist/watchlist_container';
 
-const DashboardSidebar = ({ stocks, holdings, watchlist }) => {
+const DashboardSidebar = ({ stocks, holdings, watchlist, portfolioValue }) => {
 
   return(
     <section className='dashboard-sidebar'>
         <Portfolio
+          portfolioValue={portfolioValue}
           holdings={holdings}
         />
         <Watchlist
