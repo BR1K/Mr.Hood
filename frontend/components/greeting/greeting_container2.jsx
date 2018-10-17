f4ca05d3b9d1901a65a9c22a4826f12b36a59623
Brikend Behrami <brikendbehrami@gmail.com>
2018-10-17 16:21:47 -0400

    import login action

--- frontend/components/greeting/greeting_container2.jsx
+++ frontend/components/greeting/greeting_container2.jsx
@@ -1,15 +1,16 @@
 import { connect } from 'react-redux';
-import { logout }  from '../../actions/session_actions';
+import { logout, login }  from '../../actions/session_actions';
 import Greeting2    from './greeting2';
 
 const mapStateToProps = ({ session, entities: { users } }) => {
   return {
-    currentUser: users[session.id]
+    currentUser: users[session.id],
   };
 };
 
 const mapDispatchToProps = dispatch => ({
-  logout: () => dispatch(logout())
+  logout: () => dispatch(logout()),
+  login: (user) => dispatch(login(user))
 });
 
 export default connect(
