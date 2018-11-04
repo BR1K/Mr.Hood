1472d1f4971283b163761bf3e9ce6329e07e98cd
Brikend Behrami <brikendbehrami@gmail.com>
2018-11-04 13:30:37 -0500

    remove comments

--- frontend/components/charts/dashboard_chart/dashboard_chart.jsx
+++ frontend/components/charts/dashboard_chart/dashboard_chart.jsx
@@ -11,9 +11,6 @@ class DashboardChart extends React.Component {
     }
   }
 
-
-
-
   render() {
     const data = Object.values(this.props.data).map(snapshot => {
       return snapshot.value;
@@ -28,9 +25,10 @@ class DashboardChart extends React.Component {
       <div className='stock-chart'>
 
         <div className="chart">
+
           <div className="chart-header">
-            <div className="stock-chart-price">{currencyFormatter.format(this.props.livePortfolioValue)}</div>
-            <div className="percent-change">
+            <div className="stock-chart-price">
+              {currencyFormatter.format(this.props.livePortfolioValue)}
             </div>
           </div>
 
@@ -47,9 +45,9 @@ class DashboardChart extends React.Component {
                 hide={true}
                 domain={[min, max]}
               />
-
             </LineChart>
           </ResponsiveContainer>
+
         </div>
         <br/>
         </div>
@@ -58,79 +56,3 @@ class DashboardChart extends React.Component {
 
 }
 export default DashboardChart;
-
-
-// <Tooltip
-//   wrapperStyle={{background: 'transparent', border: 'none', color: 'lightgray'}}
-//   cursor={{strokeWidth: 1}}
-//   offset={-90}
-//   isAnimationActive={false}
-//   position={{x: 0, y: 0 }}
-// />
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import { LineChart, XAxis, YAxis, CartesianGrid, Line} from 'recharts';
-//
-// const DashboardChart = ({ data }) => {
-//
-//   // const data = [
-//   // { name: 'Page A', value: 1000, pv: 2400, amt: 2400, uvError: [75, 20] },
-//   // { name: 'Page B', value: 300, pv: 4567, amt: 2400, uvError: [90, 40] },
-//   // { name: 'Page C', value: 280, pv: 1398, amt: 2400, uvError: 40 },
-//   // { name: 'Page D', value: 200, pv: 9800, amt: 2400, uvError: 20 },
-//   // { name: 'Page E', value: 278, pv: null, amt: 2400, uvError: 28 },
-//   // { name: 'Page F', value: 189, pv: 4800, amt: 2400, uvError: [90, 20] },
-//   // { name: 'Page G', value: 189, pv: 4800, amt: 2400, uvError: [28, 40] },
-//   // { name: 'Page H', value: 189, pv: 4800, amt: 2400, uvError: 28 },
-//   // { name: 'Page I', value: 189, pv: 4800, amt: 2400, uvError: 28 },
-//   // { name: 'Page J', value: 189, pv: 4800, amt: 2400, uvError: [15, 60] },
-//   // ];
-//     let arr = data.map(d => d.value);
-//     let min = Math.min(...arr);
-//     let max = Math.max(...arr);
-//
-//   return (
-//     <div>
-//       <h2>Dashboard Chart</h2>
-//
-//       <LineChart width={500} height={300} data={data}>
-//         <XAxis dataKey="name" dot={false} />
-//         <YAxis domain={[min - min*.05, max + max*.05]}/>
-//         <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-//
-//         <Line type="linear" dot={false} dataKey="value" stroke="#82ca9d" />
-//       </LineChart>
-//     </div>
-//
-//
-//   );
-//
-//
-// };
-//
-// export default DashboardChart;
